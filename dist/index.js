2c366f9ac504f4347539cc4bef40dad5748d4a73
widrns15 <85780501+widrns15@users.noreply.github.com>
2023-07-27 21:38:46 +0900

    🥽 test: TypeScript Class를 사용하여 간단한 사전 만들어보기

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,25 @@
-const sum = (a, b) => {
-    return a + b;
-};
-console.log(sum(10, 20));
+class Dict {
+  constructor() {
+    this.words = {};
+  }
+  add(word) {
+    if (this.words[word.term] === undefined) {
+      this.words[word.term] = word.def;
+    }
+  }
+  def(term) {
+    return this.words[term];
+  }
+}
+class Word {
+  constructor(term, def) {
+    this.term = term;
+    this.def = def;
+  }
+}
+const kimchi = new Word("kimchi", "한국의 매운 음식");
+const pizza = new Word("pizza", "서양의 맛있는 음식");
+const dict = new Dict();
+dict.add(kimchi);
+dict.add(pizza);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,32 @@
-const sum = (a: number, b: number): number => {
-  return a + b;
+type Words = {
+  [key: string]: string;
 };
 
-console.log(sum(10, 20));
+class Dict {
+  private words: Words;
+  constructor() {
+    this.words = {};
+  }
+  add(word: Word) {
+    if (this.words[word.term] === undefined) {
+      this.words[word.term] = word.def;
+    }
+  }
+  def(term: string) {
+    return this.words[term];
+  }
+}
+
+class Word {
+  constructor(
+    public term: string,
+    public def: string
+  ) {}
+}
+
+const kimchi = new Word("kimchi", "한국의 매운 음식");
+const pizza = new Word("pizza", "서양의 맛있는 음식");
+const dict = new Dict();
+
+dict.add(kimchi);
+dict.add(pizza);
